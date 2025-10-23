3f56a41208bb972a46a0154215c548c4314194a3
MG_Kalinin <1953115@gmail.com>
2025-10-24 00:42:55 +0300

    added metods

--- api/client.py
+++ api/client.py
@@ -1,17 +1,38 @@
+import requests
+
 class APIClient:
+    BASE_URL = "https://jsonplaceholder.typicode.com"
 
-    def __init__(self):
-        self.user_id=
-        self.title=
-        self.body=
-'''Метод возвращает список пользователей'''
+    def __init__(self,user_id=None,title=None,body=None):
+        self.user_id=user_id
+        self.title=title
+        self.body=body
+
     def get_users(self):
+        """Метод возвращает список пользователей"""
+        url = f"{self.BASE_URL}/users"
+        response = requests.get(url)
+        response.raise_for_status()  # выбросит исключение при ошибке HTTP
+        return response.json()
 
-
-'''Метод возвращает  данные конкретного пользователя'''
     def get_user(self,user_id):
+        """Метод возвращает данные конкретного пользователя"""
+        url = f"{self.BASE_URL}/users{user_id}"
+        response = requests.get(url)
+        if response.status_code == 404:
+            print(f"Пользователь с id={user_id} не найден.")
+            return None
+        response.raise_for_status()
+        return response.json()
 
-
-'''Метод создаёт пост (возвращает созданный объект с id)'''
     def create_post(self,title, body, user_id):
-
+        """Метод создаёт пост (возвращает созданный объект с id)"""
+        url = f"{self.BASE_URL}/posts"
+        payload = {
+            "title": title,
+            "body": body,
+            "userId": user_id
+        }
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        return response.json()
